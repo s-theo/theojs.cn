93605115bfc7455d9635bfb67b963fdcf44cc590
Theo <fanxiaobin422@gmail.com>
2025-11-12 12:16:27 +0800

    chore: update promo

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,8 +1,11 @@
+/// <reference types="vite/client" />
+import type { EnhanceAppContext } from 'vitepress'
+
 import DefaultTheme from 'vitepress/theme'
 
 import { h } from 'vue'
 
-import { Footer, Links, Underline } from '@theojs/lumen'
+import * as lm from '@theojs/lumen'
 import '@theojs/lumen/style'
 
 import { Footer_Data } from '../data'
@@ -11,11 +14,16 @@ export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
-      'layout-bottom': () => h(Footer, { Footer_Data })
+      'layout-bottom': () => h(lm.Footer, { Footer_Data })
     })
   },
-  enhanceApp: ({ app }) => {
-    app.component('Home', Underline)
-    app.component('Links', Links)
+  enhanceApp: ({ app }: EnhanceAppContext) => {
+    lm.umamiAnalytics({
+      id: import.meta.env.VITE_UMAMI_ID,
+      src: import.meta.env.VITE_UMAMI_SRC,
+      domains: 'theojs.cn'
+    })
+    app.component('Home', lm.Underline)
+    app.component('Links', lm.Links)
   }
 }
